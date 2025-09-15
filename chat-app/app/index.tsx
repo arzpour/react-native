bd69b7c27bc9376d7c2222871d0556092b8eed8b
z.arzpour <arzpourr@gmail.com>
2025-09-15 14:29:01 +0330

    Add theme, style, types, images and logo

--- chat-app/app/index.tsx
+++ chat-app/app/index.tsx
@@ -1,7 +1,33 @@
-import { StyleSheet, View } from "react-native";
+import { colors } from "@/constants/theme";
+import { StatusBar, StyleSheet, View } from "react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
 
-export default function HomeScreen() {
-  return <View>HomeScreen</View>;
+export default function SplashScreen() {
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={colors.neutral900}
+      />
+      <Animated.Image
+        source={require("../assets/images/splashImage.png")}
+        entering={FadeInDown.springify(700)}
+        style={styles.logo}
+        resizeMode={"contain"}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.neutral900,
+  },
+  logo: {
+    height: "23%",
+    aspectRatio: 1,
+  },
+});
