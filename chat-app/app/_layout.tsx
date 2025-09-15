d805e4ed0dff61157d89490bf16dd229688e1dd2
z.arzpour <arzpourr@gmail.com>
2025-09-15 13:22:16 +0330

    Install react native and structure for chat app

---
+++ chat-app/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
+}
---
+++ chat-app/app/index.tsx
@@ -0,0 +1,7 @@
+import { StyleSheet, View } from "react-native";
+
+export default function HomeScreen() {
+  return <View>HomeScreen</View>;
+}
+
+const styles = StyleSheet.create({});
